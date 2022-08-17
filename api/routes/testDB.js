3982fdd41e0ab3dab43189454f97c4f617fe46e9
Vinícius Ferreira <viniciusvasconcelos.ferreira@gmail.com>
2022-08-17 18:45:26 -0300

    Comentários sobre as configurações usadas na montagem dos containers

--- api/routes/testDB.js
+++ api/routes/testDB.js
@@ -10,7 +10,7 @@ router.get("/", function (req, res, next) {
 
 // Conectando com o mongoDB
 mongoose.connect(
-    "mongodb://127.0.0.1:27017/test",
+    "mongodb://172.24.33.54:27017/test?directConnection=true",
     {
         useNewUrlParser: true
     }
